7d7437289a1c4f844d96c71ccf06d77d722e1e04
Andreas Larssamils <larssamils.andreas@gmail.com>
2022-07-29 07:05:17 +0800

    commit

---
+++ scripts/forecast.js
@@ -0,0 +1,32 @@
+const key = '96A0FfITDsrX5I4k3WugBgyHl0htRRQd';
+
+const getWeather = async (id) => {
+  const base = `http://dataservice.accuweather.com/currentconditions/v1/`;
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query, { mode: 'cors' });
+  const data = await response.json();
+
+  return data[0];
+};
+
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query, { mode: 'cors' });
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('nyhammar')
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
